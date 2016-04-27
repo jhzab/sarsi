362339a7c09aa9124e4dd97b0969c23202a50d0d
Jan-Hendrik Zab <jan@jhz.name>
2016-04-27 14:04:39 +0200

    Do not go through the shell to execute sbt

--- sarsi-sbt/Main.hs
+++ sarsi-sbt/Main.hs
@@ -8,7 +8,7 @@ import Sarsi (getBroker, getTopic)
 import Sarsi.Producer (produce)
 import System.Environment (getArgs)
 import System.Exit (ExitCode, exitWith)
-import System.Process (StdStream(..), shell, std_in, std_out)
+import System.Process (StdStream(..), proc, std_in, std_out)
 import System.Process.Machine (ProcessMachines, callProcessMachines)
 import System.IO (BufferMode(NoBuffering), hSetBuffering, stdin, stdout)
 import System.IO.Machine (byChunk)
@@ -25,14 +25,14 @@ mStdOut_ :: ProcessT IO a b -> ProcessMachines a a0 k0 -> IO ()
 mStdOut_ mp (_, Just stdOut, _)  = runT_ $ mp <~ stdOut
 mStdOut_ _  _                    = return ()
 
-producer :: String -> ProcessT IO Event Event -> IO (ExitCode)
-producer cmd sink = do
+producer :: String -> [String] -> ProcessT IO Event Event -> IO (ExitCode)
+producer cmd args sink = do
   (ec, _) <- callProcessMachines byChunk createProc (mStdOut_ pipeline)
   return ec
     where
       pipeline = sink <~ eventProcess <~ echoText stdout
       echoText h = autoM $ (\txt -> TextIO.hPutStr h txt >> return txt)
-      createProc  = (shell cmd) { std_in = Inherit, std_out = CreatePipe }
+      createProc  = (proc cmd args) { std_in = Inherit, std_out = CreatePipe }
 
 main :: IO ()
 main = do
@@ -41,5 +41,5 @@ main = do
   args  <- getArgs
   b     <- getBroker
   t     <- getTopic b "."
-  ec    <- produce t $ producer $ concat $ List.intersperse " " ("sbt":args)
+  ec    <- produce t $ producer "sbt" args
   exitWith ec
